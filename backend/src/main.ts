a1ff1ef30a4e41eb1506254db7dde17ca0ce6883
Guilherme-C-Jesus <guilhermejesus@biofy.tech>
2025-09-30 02:23:09 -0300

    feat: adição de filtro global para tratamento de exceções HTTP na aplicação

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Brain Agriculture API')
@@ -27,4 +29,4 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
