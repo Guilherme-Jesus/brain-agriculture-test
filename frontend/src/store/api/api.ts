d29060cec14a8b992aa6415e144263c8d74d8d19
Guilherme-C-Jesus <guilhermejesus@biofy.tech>
2025-10-03 14:06:47 -0300

    feat: atualização do package.json para incluir novos scripts de build e ajuste da base URL da API para usar variável de ambiente

--- frontend/src/store/api/api.ts
+++ frontend/src/store/api/api.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL,
+  }),
   tagTypes: [
     'Producer',
     'Farm',
